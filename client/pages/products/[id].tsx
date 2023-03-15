6e52e5ce82b24bcc7a137ccf4883b78d352b9fba
Maxime <maxime.doury@gmail.com>
2023-03-15 09:01:57 +0100

    feat: Product pages template

--- client/pages/products/[id].tsx
+++ client/pages/products/[id].tsx
@@ -2,11 +2,82 @@ import { gql } from "@apollo/client";
 import { client } from "gql/client";
 import type { GetProductIdsQuery, GetProductQuery } from "gql/graphql";
 import type { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { FormEvent, useCallback, useState } from "react";
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
-export default function ProductPage(props: Props) {
-  return <pre>{JSON.stringify(props.product, null, 2)}</pre>;
+async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+  e.preventDefault();
+  if ("quantity" in e.currentTarget) {
+    console.log({ quantity: e.currentTarget.quantity.value });
+  }
+}
+
+export default function ProductPage({ product }: Props) {
+  const [quantity, setQuantity] = useState(1);
+  const incrementQuantity = useCallback(() => {
+    setQuantity((quantity) => quantity + 1);
+  }, []);
+  const decrementQuantity = useCallback(() => {
+    setQuantity((quantity) => Math.max(0, quantity - 1));
+  }, []);
+
+  return (
+    <article>
+      <section>
+        <figure>
+          <img src={product.img_url} />
+        </figure>
+        <h1>{product.name}</h1>
+        <span>
+          {product.power} // Packet of {product.quantity}
+        </span>
+        <form onSubmit={handleSubmit}>
+          <span>£{product.price / 100}</span>
+          <span>
+            <button type="button" onClick={decrementQuantity}>
+              -
+            </button>
+            <label>
+              <span>Qty</span>
+              <input
+                name="quantity"
+                type="number"
+                onChange={(e) => setQuantity(parseInt(e.target.value))}
+                min={0}
+                value={quantity}
+              />
+            </label>
+            <button type="button" onClick={incrementQuantity}>
+              +
+            </button>
+          </span>
+          <button>Add to cart</button>
+        </form>
+      </section>
+      <section>
+        <h2>Description</h2>
+        <p>{product.description}</p>
+      </section>
+      <section>
+        <h2>Specifications</h2>
+        <div>
+          <span>Brand</span>
+          <span>{product.brand}</span>
+          <span>Item weight</span>
+          <span>{product.weight}</span>
+          <span>Dimensions</span>
+          <span>
+            {product.height} x {product.width} x {product.length}
+          </span>
+          <span>Item model number</span>
+          <span>{product.model_code}</span>
+          <span>Colour</span>
+          <span>{product.colour}</span>
+        </div>
+      </section>
+    </article>
+  );
 }
 
 export async function getStaticPaths() {
--- client/pages/products/index.tsx
+++ client/pages/products/index.tsx
@@ -2,11 +2,26 @@ import { gql } from "@apollo/client";
 import { client } from "gql/client";
 import type { GetProductsQuery } from "gql/graphql";
 import type { InferGetStaticPropsType } from "next";
+import Link from "next/link";
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
-export default function ProductListPage(props: Props) {
-  return <pre>{JSON.stringify(props.products, null, 2)}</pre>;
+export default function ProductListPage({ products }: Props) {
+  return (
+    <main>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <figure>
+              <img src={product.img_url} alt={product.name} />
+            </figure>
+            <h2>{product.name}</h2>
+            <Link href={`/products/${product.id}`}>Discover more</Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 export async function getStaticProps() {
