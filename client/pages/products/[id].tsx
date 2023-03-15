ac6bd773e9f171c98ba8c7626cd3374da6edd493
Maxime <maxime.doury@gmail.com>
2023-03-15 08:21:28 +0100

    feat: Enable SSG for product info and list pages
    - Fetch data necessary for both pages

--- client/pages/products/[id].tsx
+++ client/pages/products/[id].tsx
@@ -1,3 +1,53 @@
-export default function ProductPage() {
-  return <div>Product page</div>;
+import { gql } from "@apollo/client";
+import { client } from "gql/client";
+import type { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+export default function ProductPage(props: Props) {
+  return <pre>{JSON.stringify(props.product, null, 2)}</pre>;
+}
+
+export async function getStaticPaths() {
+  const { data } = await client.query({
+    query: gql`
+      query GetProductIds {
+        allProducts {
+          id
+        }
+      }
+    `,
+  });
+  return {
+    paths: data.allProducts.map(({ id }) => ({ params: { id } })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const { data } = await client.query({
+    query: gql`
+      query GetProduct($id: ID!) {
+        Product(id: $id) {
+          id
+          name
+          description
+          img_url
+          price
+          power
+          quantity
+          brand
+          weight
+          height
+          width
+          length
+          model_code
+          colour
+        }
+      }
+    `,
+    variables: { id: context.params.id },
+  });
+
+  return { props: { product: data.Product } };
 }
--- client/pages/products/index.tsx
+++ client/pages/products/index.tsx
@@ -1,3 +1,31 @@
-export default function Product() {
-  return <div>Product list page</div>;
+import { gql } from "@apollo/client";
+import { client } from "gql/client";
+import { InferGetStaticPropsType } from "next";
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+export default function ProductListPage(props: Props) {
+  return <pre>{JSON.stringify(props.products, null, 2)}</pre>;
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query GetProducts {
+        allProducts {
+          id
+          img_url
+          height
+          name
+          price
+          width
+        }
+      }
+    `,
+  });
+  return {
+    props: {
+      products: data.allProducts,
+    },
+  };
 }
